de948a22e60d1e4491f593403f9318059c295b2e
quickhireagency <info@quickhire.agency>
2026-03-03 09:12:18 -0500

    Update config.js

--- config/config.js
+++ config/config.js
@@ -1,4 +1,4 @@
 // /config/config.js
 window.QH_CONFIG = {
-  API_BASE: "https://script.google.com/macros/s/AKfycbwE5M0VVnOl7S1E2S2EghdlboFpGx23GgpzxK08X4JRV7Ykj9pGMsKOfez3wzySXYuKAg/exec",
+  API_BASE: "https://quickhire-jobboard-proxy.vercel.app/exec",
 };
